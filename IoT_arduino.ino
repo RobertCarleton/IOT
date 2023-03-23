c0fd00b294f5eb4da163adf65e83fb3a26e54296
SkrivanekPeter <123364568+SkrivanekPeter@users.noreply.github.com>
2023-03-23 09:24:26 +0000

    Update IoT_arduino.ino

--- IoT_arduino.ino
+++ IoT_arduino.ino
@@ -1,25 +1,3 @@
-/*
-  Analog input, analog output, serial output
-
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
-
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-*/
-
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 9; // Analog output pin that the LED is attached to
@@ -34,8 +12,8 @@ double outputValue = 0;        // value output to the PWM (analog out)
 rgb_lcd lcd;
 
 const int colorR = 255;
-const int colorG = 0;
-const int colorB = 0;
+const int colorG = 255;
+const int colorB = 255;
 
 void setup() {
   pinMode(4, OUTPUT);
@@ -65,7 +43,7 @@ void loop() {
   Serial.print("\t output = ");
   Serial.println(outputValue);
 
-  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // wait 50 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(50);
 
@@ -74,14 +52,14 @@ void loop() {
    {
     lcd.setCursor(0, 0);
     lcd.print("humidity high  ");
-    lcd.setRGB(255, 0, 0);
+    lcd.setRGB(colorR, 0, 0);
    }
   else
-  {
+   {
     lcd.setCursor(0, 0);
     lcd.print("humidity normal");
-    lcd.setRGB(0, 255, 0);
-  }
+    lcd.setRGB(0, colorG, 0);
+   }
     
   lcd.setCursor(0, 1);
     // print the number of seconds since reset:
